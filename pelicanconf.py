cc1ded357ec97ce7eb64e953a8aefd3aff3c94d3
christopher_bonnett <c.bonnett@gmail.com>
2016-04-23 17:18:49 +0200

    final draft 0.3

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,8 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Christopher Bonnett'
-SITENAME = u'MK47'
-SITEURL = ''
+SITENAME = u'PL49'
+SITEURL = 'http://cbonnett.github.io/'
 
 PATH = 'content'
 STATIC_PATHS = ['images', 'pdfs'] 
@@ -19,6 +19,10 @@ DEFAULT_LANG = u'en'
  
 THEME = '/Users/Christopher_old/ice/github_code/pelican-themes/octopress'
 
+MENUITEMS = [('Archives', '/archives.html'),
+             ('LinkedIn', 'https://www.linkedin.com/in/cbonnett')]
+NEWEST_FIRST_ARCHIVES = False
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -30,8 +34,15 @@ AUTHOR_FEED_RSS = None
 # Social widget
 #SOCIAL = ('https://twitter.com/cbonnett', '#')
 TWITTER_USER = 'cbonnett'
+TWITTER_TWEET_BUTTON = True
+TWITTER_LATEST_TWEETS = True
+TWITTER_FOLLOW_BUTTON = True
 DEFAULT_PAGINATION = 10
 DISQUS_SITENAME = "https://mk49.disqus.com"
+
+# RSS/Atom feeds
+FEED_DOMAIN = SITEURL
+FEED_ATOM = 'atom.xml'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
